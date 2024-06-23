0ab88d8d3bf68dce435548d4482a654db9af529a
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-06-23 06:28:29 -0700

    hopefully these animations work

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,11 @@ module.exports = {
         "gradient-conic":
         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-				fade: 'fadeIn .3s ease-in-out',
-			},
-
-			keyframes: {
-				fadeIn: {
-					from: { opacity: 0 },
-					to: { opacity: 1 },
-				},
-			},
-
       keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
         dropInBounce: {
           '0%': { transform: 'translateY(-100vh)' },
           '80%': { transform: 'translateY(0)' },
@@ -34,20 +27,15 @@ module.exports = {
           '0%': { transform: 'translateX(-100%)', opacity: 0 },
           '100%': { transform: 'translateX(0)', opacity: 1 },
         },
-      },
-
-      animation: {
-        dropInBounce: 'dropInBounce 2s ease-out',
-        slideInLeft: 'slideInLeft 1s ease-out forwards 1.5s',
-      },
-
-      keyframes: {
         float: {
           '0%': { transform: 'translateX(-60vw)' },
           '100%': { transform: 'translateX(100vw)' },
         },
       },
       animation: {
+        fade: 'fadeIn .3s ease-in-out',
+        dropInBounce: 'dropInBounce 2s ease-out',
+        slideInLeft: 'slideInLeft 1s ease-out forwards 1.5s',
         float: 'float 15s linear infinite',
       },
     },
