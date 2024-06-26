2f55e8b05731079491b1ff3aef103263c9337a02
JojayD <professional.josephd@gmail.com>
2024-06-26 01:44:22 -0700

    Update Dockerfile and app.py for backend server configuration

--- backend/app.py
+++ backend/app.py
@@ -80,5 +80,6 @@ def give_score():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
