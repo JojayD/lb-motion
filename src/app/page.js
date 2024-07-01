85e067f11d5bce753e13a2a8700dbfd5d9ac685e
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2024-07-01 01:13:41 -0700

    adjust responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -16,9 +16,9 @@ export default function Home() {
       <Navbar />
       <div className="relative flex flex-col items-center justify-center min-h-screen" style={{ backgroundColor: '#BCF8D0' }}>
         <TextSphere />
-        <div className="relative z-10 flex flex-col items-center bg-white px-6 py-3 rounded-md bg-opacity-100">
-          <h1 className="text-4xl font-bold mb-6">Welcome to Lingo AI</h1>
-          <p className="text-xl mb-6">Improve your fluency today!</p>
+        <div className="relative z-10 flex flex-col items-center bg-white px-6 py-3 rounded-md bg-opacity-100 mx-6">
+          <h1 className="text-4xl font-bold mb-6 md:text-2xl">Welcome to Lingo AI</h1>
+          <p className="text-xl mb-6 md:text-l">Improve your fluency today!</p>
           <button
             onClick={handleGetStarted}
             className="bg-teal-600 text-white px-6 py-3 rounded-md hover:bg-teal-700 transition duration-300 bg-opacity-100"
