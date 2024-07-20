9a2cd7ffb35cbb104d63532d497374ed1fc527ee
JojayD <professional.josephd@gmail.com>
2024-07-19 20:34:30 -0700

    chore: Refactor Scenerios component and update variable names
    Refactor the Scenerios component to fix typos and improve code readability. Update variable names for clarity and consistency.

--- src/app/scenerios/page.js
+++ src/app/scenerios/page.js
@@ -2,21 +2,21 @@
 import React, { useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 import restaurantPrompts from "../data/restauraunts";
-function Scenerios() {
+function Scenarios() {
 	const [prompts, setPrompts] = useState(restaurantPrompts);
-	const [promptIndex, setPromptIndex] = useState(0);
-
+	const [currentIndex, setCurrentIndex] = useState(0); // Use state to handle the index
+	// console.log(prompts[1]);
 	const handleNext = () => {
-		console.log("next");
-		if (promptIndex < prompts.length - 1) {
-			setPromptIndex(promptIndex + 1);
+		if (currentIndex < prompts.length - 1) {
+			console.log(currentIndex, "Clicked");
+			setCurrentIndex(currentIndex + 1);
 		}
 	};
 
 	const handlePrev = () => {
-		console.log("prev");
-		if (promptIndex > 0) {
-			setPromptIndex(promptIndex - 1);
+		if (currentIndex > 0) {
+			console.log(currentIndex, "Clicked");
+			setCurrentIndex(currentIndex - 1);
 		}
 	};
 
@@ -24,16 +24,18 @@ function Scenerios() {
 		<>
 			<div className='flex flex-col mt-6'>
 				<div className='flex flex-row'>
-					<div class='flex flex-col gap-2 rounded-r-xl rounded-tl-xl bg-slate-100 p-4 text-slate-700 md:max-w-[60%] dark:bg-slate-800 dark:text-slate-300'>
-						<span class='font-semibold text-black dark:text-white'>Sharky</span>
+					<div className='flex flex-col gap-2 rounded-r-xl rounded-tl-xl bg-slate-100 p-4 text-slate-700 md:max-w-[60%] dark:bg-slate-800 dark:text-slate-300'>
+						<span className='font-semibold text-black dark:text-white'>Sharky</span>
+						{index > 0 && <span>Question {}</span>}
 						<TypeAnimation
-							sequence={[`${prompts[promptIndex]}`]}
+							key={currentIndex} // Add key to re-render
+							sequence={([prompts[currentIndex]])} // Use state value directly
 							wrapper='span'
 							cursor={false}
 							style={{ fontSize: "1rem", display: "inline-block" }}
 						/>
 					</div>
-					<div className='w-2/6 rounded-md translate absolute right-0'>
+					<div className='w-2/6 rounded-md absolute right-0'>
 						<img
 							src='shark.svg'
 							alt='scenarios'
@@ -42,23 +44,22 @@ function Scenerios() {
 					</div>
 				</div>
 			</div>
-			<div className='fixed inset-x-0 bottom-0 flex justify-center gap-4 p-4  dark:bg-slate-800'>
-				<div>
-					<button
-						onClick={handlePrev}
-						className='py-2 px-6 rounded-xl bg-[rgb(39,86,188)] hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200 text-gray-50 font-bold leading-loose transition duration-200'
-					>
-						Prev
-					</button>
-				</div>
-				<div>
-					<button onClick={handleNext}className='py-2 px-6 rounded-xl bg-[rgb(39,86,188)] hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200 text-gray-50 font-bold leading-loose transition duration-200'>
-						Next
-					</button>
-				</div>
+			<div className='fixed inset-x-0 bottom-0 flex justify-center gap-4 p-4 dark:bg-slate-800'>
+				<button
+					onClick={handlePrev}
+					className='py-2 px-6 rounded-xl bg-[rgb(39,86,188)] hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200 text-gray-50 font-bold leading-loose transition duration-200'
+				>
+					Prev
+				</button>
+				<button
+					onClick={handleNext}
+					className='py-2 px-6 rounded-xl bg-[rgb(39,86,188)] hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200 text-gray-50 font-bold leading-loose transition duration-200'
+				>
+					Next
+				</button>
 			</div>
 		</>
 	);
 }
 
-export default Scenerios;
+export default Scenarios;
